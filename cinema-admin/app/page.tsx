8a615129fad53d8daffab08cf13e859bf742886c
Sunera Jayasinghe <106053595+sunerajayasinghe@users.noreply.github.com>
2024-03-18 11:43:50 +0530

    feat: Cinema Administration Frontend Project Setup

---
+++ cinema-admin/app/page.tsx
@@ -0,0 +1,10 @@
+import { Empty } from 'antd';
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Cinema Administration',
+}
+
+const App: React.FC = () => <Empty />;
+
+export default App;
